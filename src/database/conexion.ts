0e476c234f340d50372542ab1318c79c16dc880f
Bryan Gonzalez <bg171628@gmail.com>
2025-11-19 19:14:37 -0600

    actulizacion crede 2

--- src/database/conexion.ts
+++ src/database/conexion.ts
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   "repositorio_documentos2_0", // nombre de la base
   "datacenter",               // usuario que me diste
-  "unah.2025",             // contraseña del usuario
+  "Unah%2025",             // contraseña del usuario
   {
     host: "127.0.0.1",         // usar 127.0.0.1 en lugar de localhost
     dialect: "mysql",
