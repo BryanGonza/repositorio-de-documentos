8be5b7905d9afd99dda464dd993da47edfeecc31
Bryan Gonzalez <bg171628@gmail.com>
2025-11-19 19:08:24 -0600

    actulizacion crede 1

--- dist/database/conexion.js
+++ dist/database/conexion.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("repositorio_documentos2_0", // nombre de la base
-"usuario_app", // usuario que me diste
-"contrasena123", // contraseña del usuario
+"datacenter", // usuario que me diste
+"unah.2025", // contraseña del usuario
 {
     host: "127.0.0.1", // usar 127.0.0.1 en lugar de localhost
     dialect: "mysql",
--- dist/models/server.js
+++ dist/models/server.js
@@ -64,7 +64,7 @@ class Server {
     constructor(port) {
         this.allowedOrigins = [
             "http://localhost:4200",
-            `http://${process.env.PUBLIC_IP || "3.20.85.74"}:4200`,
+            `http://${process.env.PUBLIC_IP || "10.10.63.205"}:4200`,
             process.env.FRONTEND_URL || "https://tu-dominio.com",
         ];
         this.app = (0, express_1.default)();
